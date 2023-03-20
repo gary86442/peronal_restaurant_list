522e6a68f628e5523153eef5c4acbd6ab1d40367
Leechiayu <gary86442@gmail.com>
2023-03-20 12:39:06 +0800

    feat:add bcrypt to gen hashed password to DB

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userDB = require("../../models/userDB");
 const passport = require("passport");
+const bcrypt = require("bcryptjs");
 //* 登入頁面
 router.get("/login", (req, res) => {
   res.render("login");
@@ -24,13 +25,16 @@ router.get("/register", (req, res) => {
 // TODO 註冊驗證
 router.post("/register", (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
-  userDB
-    .findOne({ email })
-    .then((user) => {
-      if (!user) {
-        userDB.create({ name, password, email });
-      }
-    })
+  userDB.findOne({ email }).then((user) => {
+    if (user) {
+      console.log("該信箱已經註冊過了！");
+      return res.render("/register", { name, email });
+    }
+  });
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hash) => userDB.create({ name, password: hash, email }))
     .then(() => res.redirect("/"))
     .catch((err) => console.log(err));
 });
