157d558b67580e3cda5f243569edceaac7fdc8f1
Leechiayu <gary86442@gmail.com>
2023-03-20 16:20:35 +0800

    feat:add msg reminder in login and register page, auto generate name if empty

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars");
 const router = require("./routes");
 const method_override = require("method-override");
 const session = require("express-session");
+const flash = require("connect-flash");
 const usePassport = require("./config/passport");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -17,6 +18,7 @@ app.set("view engine", "hbs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(method_override("_method"));
+app.use(flash());
 app.use(
   session({
     secret: "mysecret",
@@ -28,6 +30,9 @@ usePassport(app);
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 app.use(router);
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -25,26 +25,40 @@ router.get("/register", (req, res) => {
 // TODO 註冊驗證
 router.post("/register", (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return res.render("register", {
+      name,
+      email,
+      errors: "密碼與確認密碼不相同！",
+    });
+  }
+
   userDB.findOne({ email }).then((user) => {
     if (user) {
-      console.log("該信箱已經註冊過了！");
-      return res.render("/register", { name, email });
+      return res.render("register", {
+        name,
+        email,
+        errors: "該信箱已經註冊過了！",
+      });
     }
   });
+  const userName = name ? name : email.split("@")[0];
   bcrypt
     .genSalt(10)
     .then((salt) => bcrypt.hash(password, salt))
-    .then((hash) => userDB.create({ name, password: hash, email }))
+    .then((hash) => userDB.create({ name: userName, password: hash, email }))
     .then(() => res.redirect("/"))
     .catch((err) => console.log(err));
 });
 
 //*  登出
 router.get("/logout", (req, res, next) => {
+  // console.log(req.flash("success_msg"));
   req.logout((err) => {
     if (err) return next(err);
+    req.flash("success_msg", "你已經成功登出。");
+    res.redirect("/users/login");
   });
-  res.redirect("/users/login");
 });
 
 module.exports = router;
